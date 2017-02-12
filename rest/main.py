6257c2f473ad16aee9ae44ee0e09ac8c6b8ffb51
Will DeBerry <willdeberry@gmail.com>
2017-02-12 17:20:23 -0500

    Add authentication to mongo

--- rest/main.py
+++ rest/main.py
@@ -1,6 +1,8 @@
-from flask import Flask
+from flask import Flask, json
 from flask_restful import Resource, Api
 from pymongo import MongoClient
+from urllib.parse import quote_plus
+from pathlib import Path
 
 from statics import Statics
 from recipes import Recipes
@@ -11,9 +13,27 @@ app.config['MAX_CONTENT_LENGTH'] = 16777216
 api = Api( app )
 api.add_resource( Recipes, '/recipes' )
 
-Statics.mongo_client = MongoClient( connect = False )
-Statics.db = Statics.mongo_client['recipes']
-Statics.recipes = Statics.db['recipes']
+information = Path('/var/www/data/information.json')
+data = None
+user = None
+password = None
+db = None
+
+if information.exists():
+	with information.open() as f:
+		data = json.load(f)
+
+	for site in data['sites']:
+		if site['name'] == 'recipes':
+			user = site['user']
+			password = site['password']
+			db = site['db']
+
+uri = 'mongodb://{}:{}@localhost'.format(user, quote_plus(password))
+
+Statics.mongo_client = MongoClient( uri, connect = False )
+Statics.db = Statics.mongo_client[db]
+Statics.recipes = Statics.db[db]
 
 
 if __name__ == '__main__':
